d62243a7f008e2d0770ce8ef0f81881921a94064
Guilherme Adriano Schafer <gui.schafer@hotmail.com>
2018-11-04 21:56:46 -0200

    alterações no layout

---
+++ CompareRemedios/CompareRemedios.Portal.Framework/Controllers/PrecoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompareRemedios.Portal.Framework.Controllers
+{
+    public class PrecoController : Controller
+    {
+        // GET: Preco
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
